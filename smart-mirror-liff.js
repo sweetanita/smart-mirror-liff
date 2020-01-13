685918ab8515c42b0665e2fce9c1dc4f222691a9
Ukrit Pongsathaporn <ukrit.p@linecorp.com>
2020-01-13 17:34:53 +0700

    stabilising liff init

--- smart-mirror-liff.js
+++ smart-mirror-liff.js
@@ -51,12 +51,7 @@ window.onload = function (e) {
 };
 
 function initializeApp(data) {
-    document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
-    document.getElementById('useridfield').textContent = data.context.userId;
-    document.getElementById('utouidfield').textContent = data.context.utouId;
-    document.getElementById('roomidfield').textContent = data.context.roomId;
-    document.getElementById('groupidfield').textContent = data.context.groupId;
+  
 
     // openWindow call
     document.getElementById('openwindowbutton').addEventListener('click', function () {
