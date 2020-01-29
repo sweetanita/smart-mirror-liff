fcafe98b15a67d7afba94bf45e65ce6b21a03724
Ukrit Pongsathaporn <ukrit.p@linecorp.com>
2020-01-29 17:15:18 +0700

    check electron, change ngrok ur.

--- smart-mirror-liff.js
+++ smart-mirror-liff.js
@@ -3,7 +3,7 @@ window.onload = function (e) {
         initializeApp(data);
     });
     if ("WebSocket" in window) {
-        var wsURL = "wss://f0d83f15.ngrok.io?user=1"
+        var wsURL = "wss://cb0bf788.ngrok.io?user=1"
        // alert("WebSocket is supported by your Browser!");
        
        // Let us open a web socket
